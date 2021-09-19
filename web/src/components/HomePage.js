44845438c6118323a084863b3d48109a9cf49548
Jacob Duncan <jacobduncan00@yahoo.com>
2021-09-19 10:43:44 -0400

    Update HomePage.js

--- web/src/components/HomePage.js
+++ web/src/components/HomePage.js
@@ -10,7 +10,7 @@ const HomePage = () => {
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Prepare for future hurricanes now!</h1>
                         <p className="mb-8 leading-relaxed">Hurricane Damage Detector is a tool that allows users to map out previous hurricanes to study their path of destruction as well as produce accurate, live data of hurricanes forming currently. Ultimately, allowing users to prepare themselves in sufficient time if a hurricane is aligned with them.</p>
                             <div className="flex justify-center">
-                                <GitHubButton data-size="large">See Code</GitHubButton>
+                                <GitHubButton data-size="large" href="https://github.com/jacobduncan00/hackmit2021">See Code</GitHubButton>
                             </div>
                 </div>
             </div>
